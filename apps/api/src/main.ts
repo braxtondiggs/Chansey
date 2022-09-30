0e228540356c9393a6c93b4df7ea436c81dfe945
Braxton Diggs <braxtondiggs@gmail.com>
2022-09-29 23:49:29 -0400

    Add Fastify & Add Health check

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,34 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
+import compression from '@fastify/compress';
+import helmet from '@fastify/helmet';
+import fastifyCsrf from '@fastify/csrf-protection';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      ignoreTrailingSlash: true
+    })
   );
+
+  await app.register(helmet);
+  await app.register(fastifyCsrf);
+  await app.register(compression);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0', () => {
+    if (process.env.NODE_ENV !== 'production') {
+      Logger.log(`🚀 Application is running on: http://localhost:${port}/api`);
+    }
+  });
 }
 
 bootstrap();
