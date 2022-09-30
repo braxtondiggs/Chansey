526162e799303fef0822b4c4bf75026003ef7314
Braxton Diggs <braxtondiggs@gmail.com>
2022-09-29 23:49:29 -0400

    Add Fastify & Add Health check

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { HealthModule } from '../health/health.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
