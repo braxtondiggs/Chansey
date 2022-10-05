98c130b1b848044821a4c54a7baeb59b7fc46e3e
Braxton Diggs <braxtondiggs@gmail.com>
2022-10-04 21:12:10 -0400

    Connect Database

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { HealthModule } from '../health/health.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrmModule } from '../orm.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    OrmModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
