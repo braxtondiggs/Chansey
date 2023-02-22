be01e12ea69857c100539552187358dd8bb58ac8
Braxton Diggs <braxtondiggs@gmail.com>
2023-02-22 15:35:50 -0500

    update health check

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -1,22 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import {
+  DiskHealthIndicator,
+  HealthCheck,
   HealthCheckService,
+  HttpHealthIndicator,
   MemoryHealthIndicator,
-  HealthCheck
+  MikroOrmHealthIndicator
 } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
   constructor(
+    private readonly disk: DiskHealthIndicator,
     private readonly health: HealthCheckService,
-    private readonly memory: MemoryHealthIndicator
+    private readonly http: HttpHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
+    private readonly mikroOrm: MikroOrmHealthIndicator
   ) { }
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([
-      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024)
+      () => this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+      () => this.http.pingCheck('coingecko', 'https://api.coingecko.com/api/v3/ping'),
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+      () => this.mikroOrm.pingCheck('mikroOrm'),
     ]);
   }
 }
