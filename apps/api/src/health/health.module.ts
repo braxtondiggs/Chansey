bf945bc2fe41a9ed497fe96ef9b75b9d631c6901
Braxton Diggs <braxtondiggs@gmail.com>
2023-02-22 15:35:50 -0500

    update health check

--- apps/api/src/health/health.module.ts
+++ apps/api/src/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [HttpModule, TerminusModule],
   controllers: [HealthController]
 })
 export class HealthModule { }
