150eae600d0455af0342128862ddb50ed40f5b91
Braxton Diggs <braxtondiggs@gmail.com>
2023-07-12 21:37:43 -0400

    add order history and validation

--- apps/api/src/order/order.module.ts
+++ apps/api/src/order/order.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { OrderController } from './order.controller';
+import { Order } from './order.entity';
 import { OrderService } from './order.service';
 import User from '../users/users.entity';
 import UsersService from '../users/users.service';
@@ -9,7 +10,7 @@ import UsersService from '../users/users.service';
 @Module({
   controllers: [OrderController],
   exports: [OrderService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([Order, User])],
   providers: [OrderService, UsersService]
 })
 export class OrderModule {}
