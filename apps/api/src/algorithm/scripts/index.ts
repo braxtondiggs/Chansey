5a27e37d45e8fe690cf3637d4b6acd1af9132437
Braxton Diggs <braxtondiggs@gmail.com>
2023-10-27 14:31:12 -0400

    service rename

--- apps/api/src/algorithm/scripts/index.ts
+++ apps/api/src/algorithm/scripts/index.ts
@@ -1,4 +1,4 @@
 export * from './exponential-moving-average.service';
-export * from './moving-average.service';
+export * from './moving-average-crossover.service';
 export * from './mean-reversion.service';
 // TODO: Make code dynamically export all services in this directory
